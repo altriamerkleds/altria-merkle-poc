a006c0b7c6b3339b7d65995af6c41f8f58146933
Naveen chauhan <naveen.chauhan@merkle.com>
2024-05-24 18:21:10 +0530

    scancode changes

--- blocks/scancode/scancode.js
+++ blocks/scancode/scancode.js
@@ -1,9 +1,8 @@
 export default function scancode(block) {
-    const inputEl = [...block.firstElementChild.children];
-    console.log(inputEl[0].children[0].innerHTML);
-    var container = document.querySelector(".scancode");
-    var scinput = document.createElement("input");
-    container.firstElementChild.append(scinput);
-    scinput.placeholder=inputEl[0].children[0].innerHTML
-    inputEl[0].children[0].classList.add('input-text')
+let container = document.querySelector(".scancode");
+let scinput = document.createElement("input");
+const inputEl = [...block.firstElementChild.children];
+container.firstElementChild.append(scinput);
+scinput.placeholder=inputEl[0].children[0].innerHTML;
+inputEl[0].children[0].classList.add('input-text');
 }
