0cdd9566d1190cc46331e8626f3611d9901a8bb2
Bhupesh Palav <bhupesh.palav@merkle.com>
2024-05-24 18:25:45 +0530

    Lint issue resolve

--- blocks/scancode/scancode.js
+++ blocks/scancode/scancode.js
@@ -1,8 +1,8 @@
 export default function scancode(block) {
-let container = document.querySelector(".scancode");
-let scinput = document.createElement("input");
-const inputEl = [...block.firstElementChild.children];
-container.firstElementChild.append(scinput);
-scinput.placeholder=inputEl[0].children[0].innerHTML;
-inputEl[0].children[0].classList.add('input-text');
+  const container = document.querySelector('.scancode');
+  const scinput = document.createElement('input');
+  const inputEl = [...block.firstElementChild.children];
+  container.firstElementChild.append(scinput);
+  scinput.placeholder = inputEl[0].children[0].innerHTML;
+  inputEl[0].children[0].classList.add('input-text');
 }
