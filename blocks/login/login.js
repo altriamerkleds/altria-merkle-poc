fc5e4e1440fd5cb7f298ffe806b9ec755de82773
sam <sumishra@merkleinc.com>
2024-07-12 17:39:35 +0530

    minor

--- blocks/login/login.js
+++ blocks/login/login.js
@@ -1,31 +1,31 @@
 export default function decorate(block) {
-    const userCredentials = [];
-    const container = document.querySelector('.login');
-    const userForm = document.createElement('form');
-    userForm.setAttribute("id", "loginForm");
-    const labelInput = document.createElement('label');
-    labelInput.innerHTML = 'User Name';
-    const userInput = document.createElement('input');
-    userInput.type = 'text';
-    userInput.name = 'username';
-    userInput.id = 'username';
-    userInput.placeholder = 'Enter a UserName';
-    const pwdLabel = document.createElement('label');
-    pwdLabel.innerHTML = 'Password';
-    const pwdInput = document.createElement('input');
-    pwdInput.type = 'password';
-    pwdInput.name = 'password';
-    pwdInput.id = 'password';
-    pwdInput.placeholder = 'Enter a Password';
-    const btnInput = document.createElement('button');
-    btnInput.type = 'submit';
-    btnInput.innerHTML = 'Login';
-    container.append(userForm);
-    userForm.appendChild(labelInput);
-    userForm.appendChild(userInput);
-    userForm.appendChild(pwdLabel)
-    userForm.appendChild(pwdInput);
-    userForm.appendChild(btnInput);
+  const userCredentials = [];
+  const container = document.querySelector('.login');
+  const userForm = document.createElement('form');
+  userForm.setAttribute("id", "loginForm");
+  const labelInput = document.createElement('label');
+  labelInput.innerHTML = 'User Name';
+  const userInput = document.createElement('input');
+  userInput.type = 'text';
+  userInput.name = 'username';
+  userInput.id = 'username';
+  userInput.placeholder = 'Enter a UserName';
+  const pwdLabel = document.createElement('label');
+  pwdLabel.innerHTML = 'Password';
+  const pwdInput = document.createElement('input');
+  pwdInput.type = 'password';
+  pwdInput.name = 'password';
+  pwdInput.id = 'password';
+  pwdInput.placeholder = 'Enter a Password';
+  const btnInput = document.createElement('button');
+  btnInput.type = 'submit';
+  btnInput.innerHTML = 'Login';
+  container.append(userForm);
+  userForm.appendChild(labelInput);
+  userForm.appendChild(userInput);
+  userForm.appendChild(pwdLabel)
+  userForm.appendChild(pwdInput);
+  userForm.appendChild(btnInput);
 
 
     // rows and columns to get the data
